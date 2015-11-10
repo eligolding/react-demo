198ad9b9759768eb16720bc14a1efc1511e834c0
Eli Golding <egolding@qualitybath.com>
2015-11-10 11:03:17 -0500

    Stateless functional components FTW

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,32 +4,28 @@ import ReactDOM from 'react-dom';
 import TwitterBox from './TwitterBox';
 import TweetList from './TweetList';
 
-const App = React.createClass({
-  render: function() {
-    return (
-      <div className="container">
-        <div className="page-header">
-          <h1>Tweeter</h1>
-        </div>
-        <div className="row">
-          <div className="col-md-3 panel panel-default">
-            <div className="clearfix" style={{padding: '10px 0'}}>
-              <div className="pull-left" style={{marginRight: '7px'}}><img src={this.props.user.avatar} /></div>
-              <div>{this.props.user.name}</div>
-            </div>
-          </div>
-          <div className="col-md-6 well">
-            <TwitterBox user={this.props.user} onTweet={this.props.addTweet} />
+export default function({user, tweets, addTweet, removeTweet}) {
+  return (
+    <div className="container">
+      <div className="page-header">
+        <h1>Tweeter</h1>
+      </div>
+      <div className="row">
+        <div className="col-md-3 panel panel-default">
+          <div className="clearfix" style={{padding: '10px 0'}}>
+            <div className="pull-left" style={{marginRight: '7px'}}><img src={user.avatar} /></div>
+            <div>{user.name}</div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-offset-3 col-md-6">
-            <TweetList tweets={this.props.tweets} onDelete={this.props.removeTweet}/>
-          </div>
+        <div className="col-md-6 well">
+          <TwitterBox user={user} onTweet={addTweet} />
         </div>
       </div>
-    );
-  }
-});
-
-export default App;
+      <div className="row">
+        <div className="col-md-offset-3 col-md-6">
+          <TweetList tweets={tweets} onDelete={removeTweet}/>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/TweetList.jsx
+++ src/components/TweetList.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
 
-const TweetList = React.createClass({
-  render: function () {
-    return (
-      <ul className="list-group">
-        {this.props.tweets.map(function (tweet) {
-          return (
-            <li key={tweet.dateTime} className="list-group-item clearfix">
-              <div className="pull-left" style={{marginRight: '10px'}}>
-                <img src={tweet.avatar} />
-              </div>
-              <p className="small">{tweet.username} {tweet.dateTime}</p>
-              <p className="lead">{tweet.msg}</p>
-              <a className="delete pull-right" onClick={this.props.onDelete.bind(null, tweet)}>delete</a>
-            </li>
-          );
-        }.bind(this))}
-      </ul>
-    )
-  }
-});
-
-export default TweetList;
+export default function({tweets, onDelete}) {
+  return (
+    <ul className="list-group">
+      {tweets.map(function (tweet) {
+        return (
+          <li key={tweet.dateTime} className="list-group-item clearfix">
+            <div className="pull-left" style={{marginRight: '10px'}}>
+              <img src={tweet.avatar} />
+            </div>
+            <p className="small">{tweet.username} {tweet.dateTime}</p>
+            <p className="lead">{tweet.msg}</p>
+            <a className="delete pull-right" onClick={onDelete.bind(null, tweet)}>delete</a>
+          </li>
+        );
+      })}
+    </ul>
+  )
+}
